ab1226e2c16317973be6537ff588b0901bfa2544
jackrakk <106267006+jackrakk@users.noreply.github.com>
2022-05-25 22:12:00 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('Join Channel', url='https://t.me/Raj_Files_backup')
+            InlineKeyboardButton('Join Channel', url='https://t.me/RFs_Movies')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
